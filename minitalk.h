08e808274c80a66d39b80b9854aea02a2f9f3899
sboukiou <sboukiou@student.1337.ma>
2025-02-07 15:18:42 +0100

    addin minitalk.h

--- minitalk.h
+++ minitalk.h
@@ -1,8 +1,11 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 #include <sys/types.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+
+/*Including dependencies -> libft + ft_printf*/
+#include "./dependencies/libft/libft.h"
+#include "./dependencies/ft_printf/ft_printf.h"
 #endif
