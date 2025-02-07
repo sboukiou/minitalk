2b285f549f76d15bc2872db95158a89123ff5d56
sboukiou <sboukiou@student.1337.ma>
2025-02-07 15:18:33 +0100

    Client , Implementing a basic encoder for bytes, needs debugging

--- client.c
+++ client.c
@@ -12,18 +12,46 @@
 
 #include "minitalk.h"
 
+void	send_byte(int byte, pid_t server_pid)
+{
+
+	int idx = 0;
+	while (idx < 8)
+	{
+		if (byte % 2)
+			kill(server_pid, idx + 15);
+		byte = byte / 2;
+		idx++;
+		sleep(0.4);
+	}
+	kill (server_pid, SIGINT);
+}
+
+void	send_message(char *message, pid_t server_pid)
+{
+	int	idx;
+
+	if (!message)
+		return ;
+	idx = 0;
+	while (message[idx])
+	{
+		send_byte(message[idx], server_pid);
+		idx++;
+	}
+	kill(server_pid, 11);
+}
+
 int main(int ac, char **av)
 {
 	int	server_pid;
 
-	if (ac != 2)
+	if (ac != 3)
 	{
-		printf("Invalid number of arguments\n");
+		ft_printf("Invalid number of arguments\n");
 		return (0);
 	}
-	server_pid = atoi(av[1]);
-	for (int i = 0; i < 33; i++)
-		kill(server_pid, i);
-	kill(server_pid, SIGINT);
+	server_pid = ft_atoi(av[1]);
+	send_message(av[2], server_pid);
 	return (0);
 }
