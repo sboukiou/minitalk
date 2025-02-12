8fd09c321351e46d4fc1d2c03b49e5604ffb98c3
sboukiou <sboukiou@student.1337.ma>
2025-02-12 11:48:20 +0100

    Restructured the header file + adding prototype for the _power func defined in utils.c

--- minitalk.h
+++ minitalk.h
@@ -1,11 +1,25 @@
 #ifndef MINITALK_H
 # define MINITALK_H
+
+/*#including needed headers for std functions*/
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
 
+
 /*Including dependencies -> libft + ft_printf*/
 #include "./dependencies/libft/libft.h"
 #include "./dependencies/ft_printf/ft_printf.h"
+
+/* Defined costum types*/
+typedef struct s_serverinfo
+{
+	int	byte;
+	int	idx;
+}	t_serverinfo;
+
+/*Prototypes for utils functions defined int --> utils.c */
+int _pow(int base, int power);
+
 #endif
